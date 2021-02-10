ad4f628ac84f7651abc851ed7dbd4aae27759210
mohammadDarAhmad <mohammadhamid757@gmail.com>
2021-02-10 19:17:38 +0200

    Make a login

--- main.js
+++ main.js
@@ -1,28 +1,55 @@
 var express = require('express');
 var app = express();
 
-
-
+// middlewares
+app.use(express.urlencoded({ extened: true }));
 app.use(express.static("public"));
-app.set("veiw engine","ejs");
-app.get('/', function (req, res) {
+app.set("view engine", "ejs");
 
-    var mysql = require('mysql');
+//Make connection to DB, the DB is PHPMyAdmin
+var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "health"
+  database: "vatrin"
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM hospital", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-});
-});
+app.get("/", (req, res) => {
+ res.render("index");
+})
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+
+
+// route for handling post requirests
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    // check for missing filds
+    if (!email || !password) {
+      res.send("Please enter all the fields");
+      return;
+    }
+
+ con.query("SELECT * FROM user  WHERE email ='" + email + "' && password=" + password + "", function (err, result, fields) {
+    if (err)
+      throw err;
+      if(result==0){
+        res.send("Invalid username or password");
+      }
+
+    console.log(result.length);
+
+      if(result.length>0){
+    res.redirect("/home");
+      }
+  });
+
+  
+  })
+
+
 
 app.listen(3000); 
