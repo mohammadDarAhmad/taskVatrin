56b63b8df70aba5762d5a0dd1539b365e9f60cf0
mohammadDarAhmad <mohammadhamid757@gmail.com>
2021-02-11 00:26:39 +0200

    Add session to make login, and make authentacion

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+const cookieSession = require("cookie-session");
+
+const authentication = require("./AuthenticationUser/authentication");
 
 // middlewares
 app.use(express.urlencoded({ extened: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+// Make a session
+app.use(
+  cookieSession({
+    keys: ["randomStringASyoulikehjudfsajk"],
+  })
+);
 
 //Make connection to DB, the DB is PHPMyAdmin
 var mysql = require('mysql');
@@ -16,20 +26,34 @@ var con = mysql.createConnection({
   database: "vatrin"
 });
 
+
+//
 app.get("/", (req, res) => {
  res.render("index");
 })
 app.get("/login", (req, res) => {
-  res.render("login");
+  var masseage = null;
+
+  res.render("login",{masseage});
 })
 
+app .get("/home", authentication, (req, res) => {
+  res.render("home", { user: req.session.user });
+});
+
+//logout
+app.get("/logout", authentication, (req, res) => {
+  req.session.user = null;
+  res.redirect("/login");
+});
 
 // route for handling post requirests
 app.post("/login", async (req, res) => {
     const { email, password } = req.body;
     // check for missing filds
     if (!email || !password) {
-      res.send("Please enter all the fields");
+      var masseage = "Please enter all the fields";
+      res.render('login.ejs',{masseage})
       return;
     }
 
@@ -37,17 +61,23 @@ app.post("/login", async (req, res) => {
     if (err)
       throw err;
       if(result==0){
-        res.send("Invalid username or password");
+        var masseage = "nvalid username or password";
+        res.render('login.ejs',{masseage})
+      return;
       }
 
     console.log(result.length);
 
       if(result.length>0){
+         
+    req.session.user = {
+      email,
+    };
     res.redirect("/home");
       }
   });
 
-  
+
   })
 
 
